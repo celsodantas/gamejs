30618e4dfa029603a185929b458de163cca861d4
Celso Dantas <celsodantas@gmail.com>
2012-04-23 21:03:49 -0300

    organization in Game.init()

--- js/core/game.js
+++ js/core/game.js
@@ -9,36 +9,14 @@ Game.prototype = {
 
 	init: function() 
 	{	
-		if( Detector.webgl ){
-			this._renderer = new THREE.WebGLRenderer({
-				antialias				: true,	// to get smoother output
-				preserveDrawingBuffer	: true	// to allow screenshot
-			});
-			this._renderer.setClearColorHex( 0x000000, 1 );
-		}else{
-			this._renderer = new THREE.CanvasRenderer();
-		}
+		this.initRenderer();
+		this.initStats();
+		this.initScene();
+		this.initCamera();
+		this.initPlayer();
 		
-		this._renderer.setSize( window.innerWidth, window.innerHeight );
-		document.getElementById('container').appendChild(this._renderer.domElement);
-
-		this._stats = new Game.Stats();
-		this._stats.init();
-
-		// create a scene
-		this._scene = new THREE.Scene();
-
-		//camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, -2000, 1000 );
-		this._camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 10000 );
-		this._camera.position.set(0, 0, 5);
-		this._camera.lookAt(new THREE.Vector3(0, 0, 0));
-		this._scene.add(this._camera);
-
 		// transparently support window resize
 		THREEx.WindowResize.bind(this._renderer, this._camera);
-		
-		// Init player
-		this._player = new Game.Player(Game.Base.SpriteSheet, Game.Base.Animation);
 		
 		return true;
 	},
@@ -61,7 +39,52 @@ Game.prototype = {
 	requestAnimationFrame: function() {
 		var self = this;
 		requestAnimationFrame( function() { self.render() });
-	}
+	},
+	
+	/*
+	*
+	*   Initializations 
+	*
+	*/
+	
+	initStats: function() {
+		// add Stats.js - https://github.com/mrdoob/stats.js
+		this._stats = new Stats();
+		this._stats.domElement.style.position	= 'absolute';
+		this._stats.domElement.style.bottom	= '0px';
+		document.body.appendChild( this._stats.domElement );
+	},
+	
+	initScene: function() {
+		this._scene = new THREE.Scene();
+	},
+	
+	initRenderer: function() {
+		if( Detector.webgl ){
+			this._renderer = new THREE.WebGLRenderer({
+				antialias				: true,	// to get smoother output
+				preserveDrawingBuffer	: true	// to allow screenshot
+			});
+			this._renderer.setClearColorHex( 0x000000, 1 );
+		}else{
+			this._renderer = new THREE.CanvasRenderer();
+		}
+		
+		this._renderer.setSize( window.innerWidth, window.innerHeight );
+		document.getElementById('container').appendChild(this._renderer.domElement);	
+	},
+	
+	initCamera: function() {
+		//camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, -2000, 1000 );
+		this._camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 10000 );
+		this._camera.position.set(0, 0, 5);
+		this._camera.lookAt(new THREE.Vector3(0, 0, 0));
+		this._scene.add(this._camera);
+	},
+	
+	initPlayer: function() {
+		this._player = new Game.Player(Game.Base.SpriteSheet, Game.Base.Animation);	
+	},
 }
 
 Game.Base = function() {};
